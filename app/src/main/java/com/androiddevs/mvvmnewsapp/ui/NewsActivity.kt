9a0c314f9d38845bfb334a10a65a5992999e3276
Amina <amina.muradalieva91@gmail.com>
2024-01-30 12:05:35 +0600

    App navigation setup. Uses bottom navigation view.

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -0,0 +1,25 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.databinding.ActivityNewsBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class NewsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navView: BottomNavigationView = binding.bottomNavigationView
+
+        val navController = findNavController(R.id.newsNavHostFragment)
+        navView.setupWithNavController(navController)
+
+    }
+}
