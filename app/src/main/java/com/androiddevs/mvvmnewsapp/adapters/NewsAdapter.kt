ef5d8bf26c2d08213a6afbed506382eb3cbe13be
Amina <amina.muradalieva91@gmail.com>
2024-01-31 11:33:44 +0600

    RecyclerView setup done with DiffUtil and AsyncListDiffer

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsAdapter.kt
@@ -0,0 +1,78 @@
+package com.androiddevs.mvvmnewsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.databinding.ItemArticlePreviewBinding
+import com.androiddevs.mvvmnewsapp.models.Article
+import com.bumptech.glide.Glide
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+    inner class ArticleViewHolder(private val binding: ItemArticlePreviewBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(article: Article) {
+            binding.apply {
+                Glide.with(this.root).load(article.urlToImage).into(ivArticleImage)
+                tvSource.text = article.source.name
+                tvTitle.text = article.title
+                tvDescription.text = article.description
+                tvPublishedAt.text = article.publishedAt
+                root.setOnClickListener{
+                    onItemClickListener?.let{it(article)}
+                }
+            }
+        }
+    }
+
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit){
+        onItemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+
+        /*return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article_preview,
+                parent,
+                false
+            )
+        )*/
+        val binding = ItemArticlePreviewBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ArticleViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.bind(article)
+
+    }
+
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer<Article>(this, differCallback)
+
+}
